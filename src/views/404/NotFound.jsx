e03cb8863bd2cc8c1bcf603b82df01a3aa15a82b
RagDo <rag.do200@gmail.com>
2023-02-18 23:21:55 +0300

    style not found page

--- src/views/404/NotFound.jsx
+++ src/views/404/NotFound.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import "./notFound.scss";
+// Component
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="notFound">
+      <Sidebar />
+      <div className="container">
+        <Navbar />
+        <main>
+          <h1>عذرا !</h1>
+          <p>الصفحة غير موجودة</p>
+        </main>
+      </div>
+    </div>
+  );
 }
